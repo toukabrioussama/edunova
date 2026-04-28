40f2fe1456377f24ca86d894ae21f1d046212735
toukabrioussama <oussama.toukabri@esprit.tn>
2026-04-28 12:12:53 +0100

    first commit test

---
+++ src/main/java/test/testclass.java
@@ -0,0 +1,13 @@
+package test;
+
+public class testclass {
+    private int test;
+
+    public int getTest() {
+        return test;
+    }
+
+    public void setTest(int test) {
+        this.test = test;
+    }
+}
